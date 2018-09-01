05ffb06ae9d046a1659095fab5926e3a79682764
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2018-09-02 01:35:51 +0800

    UPDATE: refactored main menu bar to use generic menu component

--- src/components/MainMenuBar/index.js
+++ src/components/MainMenuBar/index.js
@@ -1,80 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
 import { getRandomImage } from '../../utils/randomGenerator';
+import GenericMenu from '../GenericMenu';
 
 const styles = () => ({
 	root: {},
 });
 
 class MainMenuBar extends Component {
+
 	static defaultProps = {};
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			anchorMenuEl: null,
-		};
-	}
-
-	handleMenu = event => {
-		this.setState({ anchorMenuEl: event.currentTarget });
-	};
-
-	handleCloseMenu = () => {
-		this.setState({ anchorMenuEl: null });
-	};
 
 	handleAddCard = () => {
 		const { addACard } = this.props;
 		addACard({ title: 'Added', launched: '32', img: getRandomImage() });
-		this.handleCloseMenu();
+	};
+
+	handleExportSettings = () => {
+		console.log('exporting settings');
+	};
+
+	handleImportSettings = () => {
+		console.log('importing settings');
 	};
 
 	render() {
 		const { classes } = this.props;
-		const { anchorMenuEl } = this.state;
-		const openMenu = Boolean(anchorMenuEl);
 
 		return (
-			<div className={classes.root}>
-				<IconButton
-					aria-owns={openMenu ? 'menu-appbar' : null}
-					aria-haspopup="true"
-					onClick={this.handleMenu}
-					color="inherit">
-					<MenuIcon/>
-				</IconButton>
-
-				<Menu
-					id="menu-appbar"
-					anchorEl={anchorMenuEl}
-					anchorOrigin={{
-						vertical: 'top',
-						horizontal: 'right',
-					}}
-					transformOrigin={{
-						vertical: 'top',
-						horizontal: 'right',
-					}}
-					open={openMenu}
-					onClose={this.handleCloseMenu}>
-					<MenuItem
-						onClick={this.handleAddCard}>
-						Add Shortcut
-					</MenuItem>
-					<MenuItem onClick={this.handleCloseMenu}>
-						Export Settings
-					</MenuItem>
-					<MenuItem onClick={this.handleCloseMenu}>
-						Import Settings
-					</MenuItem>
-				</Menu>
-			</div>
+			<GenericMenu
+				className={classes.root}
+				id={`${classes.root}-id`}
+				menuItems={[
+					{
+						label: 'Add Shortcut',
+						clickAction: this.handleAddCard,
+					},
+					{
+						label: 'Export Settings',
+						clickAction: this.handleExportSettings,
+					},
+					{
+						label: 'Import Settings',
+						clickAction: this.handleImportSettings,
+					},
+				]}
+			/>
 		);
 	}
 }
