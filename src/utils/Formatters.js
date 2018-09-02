b0dfb468fefb04cfdfdb4bd4ed532be300bdb20f
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2018-09-02 09:45:53 +0800

    ADD: sort object key names alphabetically function

---
+++ src/utils/Formatters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function sortObjKeysAlphabetically(obj) {
+	let ordered = {};
+	Object.keys(obj).sort().forEach(function (key) {
+		ordered[key] = obj[key];
+	});
+	return ordered;
+}
