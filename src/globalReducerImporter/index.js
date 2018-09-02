0faca24629022054ccd206eb31f19f890176bbe8
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2018-09-02 11:11:43 +0800

    UPDATE: remove rrf from global reducer

--- src/globalReducerImporter/index.js
+++ src/globalReducerImporter/index.js
@@ -1,10 +1,8 @@
 import { sortObjKeysAlphabetically } from '../utils/Formatters';
 import rawReducers from './rawReducers';
-import reactReduxForms from './reactReduxForms';
 
 const sortedReducer = sortObjKeysAlphabetically({
 	...rawReducers,
-	...reactReduxForms,
 });
 
 export default sortedReducer;
