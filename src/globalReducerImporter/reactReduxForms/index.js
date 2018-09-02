e304c477e449c6b0572a3bd155b777ef9b581bb0
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2018-09-02 11:11:02 +0800

    UPDATE: remove rrf initial files

--- src/globalReducerImporter/reactReduxForms/index.js
+++
@@ -1,8 +0,0 @@
-import { combineForms } from 'react-redux-form';
-import dashboardForms from '../../containers/Dashboard/formReducers';
-
-export default {
-	dashboardForms: combineForms({
-		...dashboardForms
-	}, 'dashboardForms'),
-};
