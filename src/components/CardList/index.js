d96dc5188fe3fc2e8b6252de19badba01b71ce6e
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2019-09-22 09:58:31 +0800

    UPDATE: fix prettier format

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,95 +1,102 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import Avatar from '@material-ui/core/Avatar';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { styles } from './styles';
-import { preventDefaultEvent } from '../../utils/browserCommands';
-import { formatLink } from '../../utils/formatters';
-import GenericMenu from '../GenericComponents/GenericMenu';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import Avatar from "@material-ui/core/Avatar";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { styles } from "./styles";
+import { preventDefaultEvent } from "../../utils/browserCommands";
+import { formatLink } from "../../utils/formatters";
+import GenericMenu from "../GenericComponents/GenericMenu";
 
 class CardList extends Component {
+  handleCardRedirect = tile => {
+    const { updateClicker } = this.props;
+    const { link, title } = tile;
+    updateClicker(title);
+    window.location = formatLink(link);
+  };
 
-	handleCardRedirect = (tile) => {
-		const { updateClicker } = this.props;
-		const { link, title } = tile;
-		updateClicker(title);
-		window.location = formatLink(link);
-	};
+  handleEdit = card => {
+    console.log("editing", card);
+  };
 
-	handleEdit = (card) => {
-		console.log('editing', card);
-	};
+  handleDelete = card => {
+    const { removeACard } = this.props;
+    removeACard(card.title);
+  };
 
-	handleDelete = (card) => {
-		const { removeACard } = this.props;
-		removeACard(card.title);
-	};
-
-	render() {
+  render() {
     const { classes, cardList } = this.props;
 
     return (
       <div className={classes.root}>
         <GridList cellHeight={180} cols={8} className={classes.gridList}>
-	        {cardList.map((tile, indexId) => {
-		        const doesImgExists = (tile && tile.hasOwnProperty('img'));
-		        return (
-			        <GridListTile key={indexId} className={classes.gridListTile}>
-				        <ButtonBase
-					        focusRipple
-					        className={classes.buttonWrapper}
-					        onClick={() => this.handleCardRedirect(tile)}
-					        focusVisibleClassName={classes.focusVisible}>
+          {cardList.map((tile, indexId) => {
+            const doesImgExists = tile && tile.hasOwnProperty("img");
+            return (
+              <GridListTile key={indexId} className={classes.gridListTile}>
+                <ButtonBase
+                  focusRipple
+                  className={classes.buttonWrapper}
+                  onClick={() => this.handleCardRedirect(tile)}
+                  focusVisibleClassName={classes.focusVisible}
+                >
+                  <div
+                    className={`${classes.avatarWrapper} ${
+                      doesImgExists ? classes.avatarWrapperHoverEffect : ""
+                    }`}
+                  >
+                    <Avatar
+                      aria-label="default-icon"
+                      className={classes.avatar}
+                    >
+                      {tile.title.substring(0, 2).toUpperCase()}
+                    </Avatar>
+                  </div>
 
-					        <div className={`${classes.avatarWrapper} ${doesImgExists ? classes.avatarWrapperHoverEffect : ''}`}>
-						        <Avatar aria-label="default-icon" className={classes.avatar}>
-							        {tile.title.substring(0, 2).toUpperCase()}
-						        </Avatar>
-					        </div>
+                  {doesImgExists && (
+                    <img
+                      src={tile.img}
+                      onDragStart={preventDefaultEvent}
+                      className={classes.image}
+                      alt={tile.title}
+                    />
+                  )}
 
-					        {doesImgExists &&
-					        <img
-						        src={tile.img}
-						        onDragStart={preventDefaultEvent}
-						        className={classes.image}
-						        alt={tile.title}
-					        />}
+                  <GridListTileBar
+                    title={tile.title}
+                    style={{ textAlign: "left" }}
+                    subtitle={
+                      <span>
+                        <FavoriteIcon /> {tile.launched}
+                      </span>
+                    }
+                  />
+                </ButtonBase>
 
-					        <GridListTileBar
-						        title={tile.title}
-						        style={{ textAlign: 'left' }}
-						        subtitle={
-							        <span>
-                      <FavoriteIcon/> {tile.launched}
-                    </span>
-						        }
-					        />
-				        </ButtonBase>
-
-				        <GenericMenu
-					        className={classes.moreOptionsIcon}
-					        menuIcon={(<MoreVertIcon/>)}
-					        id={`${classes.root}-${indexId}`}
-					        menuItems={[
-						        {
-							        label: 'Edit',
-							        clickAction: () => this.handleEdit(tile),
-						        },
-						        {
-							        label: 'Delete',
-							        clickAction: () => this.handleDelete(tile),
-						        },
-					        ]}
-				        />
-			        </GridListTile>
-		        )
-	        })}
+                <GenericMenu
+                  className={classes.moreOptionsIcon}
+                  menuIcon={<MoreVertIcon />}
+                  id={`${classes.root}-${indexId}`}
+                  menuItems={[
+                    {
+                      label: "Edit",
+                      clickAction: () => this.handleEdit(tile)
+                    },
+                    {
+                      label: "Delete",
+                      clickAction: () => this.handleDelete(tile)
+                    }
+                  ]}
+                />
+              </GridListTile>
+            );
+          })}
         </GridList>
       </div>
     );
@@ -97,7 +104,7 @@ class CardList extends Component {
 }
 
 CardList.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(CardList);
--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,18 +1,20 @@
 export function sortObjKeysAlphabetically(obj) {
-	let ordered = {};
-	Object.keys(obj).sort().forEach(function (key) {
-		ordered[key] = obj[key];
-	});
-	return ordered;
+  let ordered = {};
+  Object.keys(obj)
+    .sort()
+    .forEach(function(key) {
+      ordered[key] = obj[key];
+    });
+  return ordered;
 }
 
-export const formatLink = (link) => {
-	const httpRegex = RegExp('^https?://', 'g');
-	const isHttpProtocolFound = httpRegex.test(link);
+export const formatLink = link => {
+  const httpRegex = RegExp("^https?://", "g");
+  const isHttpProtocolFound = httpRegex.test(link);
 
-	if (isHttpProtocolFound) {
-		return link;
-	} else {
-		return `https://${link}`;
-	}
+  if (isHttpProtocolFound) {
+    return link;
+  } else {
+    return `https://${link}`;
+  }
 };
