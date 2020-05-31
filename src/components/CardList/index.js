f0dabbb28df39fb3fecf36e4366dc4a7625ae70c
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2020-05-31 18:43:23 +0800

    UPDATE: migrated grid logic to react grid layout instead

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useState, useMemo } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import ButtonBase from "@material-ui/core/ButtonBase";
@@ -9,15 +9,15 @@ import { preventDefaultEvent } from "../../utils/browserCommands";
 import { formatLink } from "../../utils/formatters";
 import GenericMenu from "../GenericComponents/GenericMenu";
 import { GlobalStore } from "../../store/store";
-import Muuri from "muuri";
-import _isEmpty from "lodash/isEmpty";
 import _find from "lodash/find";
 import { useWindowSize } from "../../utils/lifecycle";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
 import "./styles.css";
+import GridLayout from "react-grid-layout";
 
 const CardList = ({ classes, cardList, updateClicker, removeACard }) => {
   const [width] = useWindowSize();
-  const [draggableContainer, setDraggableContainer] = useState(null);
   const [cardMouseDownTimeStamp, setCardMouseDownTimeStamp] = useState(null);
   const store = GlobalStore.useStore();
 
@@ -29,6 +29,20 @@ const CardList = ({ classes, cardList, updateClicker, removeACard }) => {
       window.close();
     },
     [updateClicker]
+  );
+
+  const handleLayoutChange = React.useCallback(
+    layout => {
+      const cardListWithLayout = cardList.map(item => {
+        const itemLayout = _find(layout, { i: item.id });
+        return {
+          ...item,
+          layout: itemLayout
+        };
+      });
+      store.set("cards")(cardListWithLayout);
+    },
+    [cardList, store]
   );
 
   const handleEdit = card => {
@@ -43,137 +57,106 @@ const CardList = ({ classes, cardList, updateClicker, removeACard }) => {
     [removeACard]
   );
 
-  useLayoutEffect(() => {
-    /**
-     * Reset muuriInstance whenever cardList changes since muuriInstance
-     * alone cannot track if new elements are added or removed
-     */
-    if (_isEmpty(cardList)) {
-      return;
-    }
+  const layoutDimensions = useMemo(() => {
+    // Don't use full width so there can be a padding on start and end tiles
+    const limitedWidth = width - 20;
+    const tileSquareDimension = Math.floor(limitedWidth / 10);
+    const colCount = Math.floor(limitedWidth / tileSquareDimension);
 
-    if (draggableContainer) {
-      draggableContainer.destroy();
-    }
-
-    const muuriInstance = new Muuri(".grid", {
-      dragEnabled: true,
-      dragStartPredicate: {
-        distance: 10,
-        delay: 100
-      }
-    });
-
-    setDraggableContainer(muuriInstance);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cardList]);
-
-  useLayoutEffect(() => {
-    if (draggableContainer) {
-      draggableContainer.layout(true);
-    }
-  }, [draggableContainer, width]);
+    return {
+      limitedWidth,
+      tileSquareDimension,
+      colCount
+    };
+  }, [width]);
 
   return (
-    <div className="grid">
-      {cardList.map((tile, indexId) => {
-        const doesImgExists = tile && tile.hasOwnProperty("imgData");
-        // Deduct the margins and borders on both sides
-        const tileSquareDimension = Math.floor(width / 10 - 6) * 0.99;
+    <>
+      <GridLayout
+        className={classes.parentContainerLayout}
+        cols={layoutDimensions.colCount}
+        rowHeight={layoutDimensions.tileSquareDimension}
+        width={layoutDimensions.limitedWidth}
+        margin={[10, 10]}
+        onLayoutChange={handleLayoutChange}
+      >
+        {cardList.map((tile, indexId) => {
+          const doesImgExists = tile && tile.hasOwnProperty("imgData");
 
-        return (
-          <div className="item" key={indexId}>
-            <div className="item-content" data-id={tile.id}>
-              <ButtonBase
-                disableRipple
-                className={classes.buttonWrapper}
-                onMouseDown={e => {
-                  e.stopPropagation();
-                  e.preventDefault();
-                  setCardMouseDownTimeStamp(e.timeStamp);
-                }}
-                onMouseUp={e => {
-                  /**
-                   * If interval between onMouseDown and onMouseUp is only short then user
-                   * initiates a click event and not drag event
-                   */
-                  e.stopPropagation();
-                  e.preventDefault();
-                  if (
-                    cardMouseDownTimeStamp &&
-                    e.timeStamp - cardMouseDownTimeStamp < 300
-                  ) {
-                    return handleCardRedirect(tile);
-                  }
-
-                  /**
-                   * We need to read data from draggableContainer then save the same order
-                   * seen on the view on the local db. We need to add a delay on saving on DB
-                   * to make the animation transition smooth
-                   */
-                  if (draggableContainer) {
-                    const sortedCardList = draggableContainer
-                      .getItems()
-                      .map(item => {
-                        const itemId = item._child.getAttribute("data-id");
-                        return _find(cardList, { id: itemId });
-                      });
-                    setTimeout(() => store.set("cards")(sortedCardList), 300);
-                  }
-                }}
-                style={{
-                  width: tileSquareDimension,
-                  height: tileSquareDimension
-                }}
-              >
-                <GridListTileBar
-                  titlePosition="top"
-                  title={tile.title}
-                  className={classes.gridListTileBar}
-                  classes={{
-                    title: classes.gridListTileTitle
+          return (
+            <div
+              className="item"
+              key={String(tile.id)}
+              data-grid={tile.layout || { x: indexId % layoutDimensions.colCount, y: 0, w: 1, h: 1 }}
+            >
+              <div className="item-content" data-id={tile.id}>
+                <ButtonBase
+                  disableRipple
+                  className={classes.buttonWrapper}
+                  onMouseDown={e => {
+                    setCardMouseDownTimeStamp(e.timeStamp);
                   }}
-                />
-
-                <div className={classes.launchedContainer}>
-                  <FavoriteIcon
-                    style={{
-                      fontSize: "inherit"
+                  onMouseUp={e => {
+                    /**
+                     * If interval between onMouseDown and onMouseUp is only short then user
+                     * initiates a click event and not drag event
+                     */
+                    e.stopPropagation();
+                    e.preventDefault();
+                    if (cardMouseDownTimeStamp && e.timeStamp - cardMouseDownTimeStamp < 300) {
+                      return handleCardRedirect(tile);
+                    }
+                  }}
+                >
+                  <GridListTileBar
+                    titlePosition="top"
+                    title={tile.title}
+                    className={classes.gridListTileBar}
+                    classes={{
+                      title: classes.gridListTileTitle
                     }}
                   />
-                  {tile.launched}
-                </div>
 
-                {doesImgExists && (
-                  <img
-                    src={tile.imgData}
-                    onDragStart={preventDefaultEvent}
-                    className={classes.image}
-                    alt={tile.title}
-                  />
-                )}
-              </ButtonBase>
+                  <div className={classes.launchedContainer}>
+                    <FavoriteIcon
+                      style={{
+                        fontSize: "inherit"
+                      }}
+                    />
+                    {tile.launched}
+                  </div>
 
-              <GenericMenu
-                className={classes.moreOptionsIcon}
-                menuIcon={<MoreVertIcon />}
-                id={`${classes.root}-${indexId}`}
-                menuItems={[
-                  {
-                    label: "Edit",
-                    clickAction: () => handleEdit(tile)
-                  },
-                  {
-                    label: "Delete",
-                    clickAction: () => handleDelete(tile)
-                  }
-                ]}
-              />
+                  {doesImgExists && (
+                    <img
+                      src={tile.imgData}
+                      onDragStart={preventDefaultEvent}
+                      className={classes.image}
+                      alt={tile.title}
+                    />
+                  )}
+                </ButtonBase>
+
+                <GenericMenu
+                  className={classes.moreOptionsIcon}
+                  menuIcon={<MoreVertIcon />}
+                  id={`${classes.root}-${indexId}`}
+                  menuItems={[
+                    {
+                      label: "Edit",
+                      clickAction: () => handleEdit(tile)
+                    },
+                    {
+                      label: "Delete",
+                      clickAction: () => handleDelete(tile)
+                    }
+                  ]}
+                />
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </GridLayout>
+    </>
   );
 };
 
